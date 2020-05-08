ce3267b9e6b9de3257bdc5f0796648607b604999
Olga <40832213+Oliaaa@users.noreply.github.com>
2020-05-09 02:49:06 +0700

    Update check_data.py

--- check_data.py
+++ check_data.py
@@ -4,51 +4,34 @@ class NotEmailError(Exception):
 class NotNameError(Exception):
     pass
 
-def registration():
-    i = 0
-    data = {}
-    with open('C:/Users/olivi/skills/HW9/registrations_.txt', 'r', encoding="utf-8") as rf:
-        for line in rf:
-            data[i] = line.split()
-            i += 1
+def check_data(line):
+    if len(line) != 3:
+        raise ValueError('НЕ присутсвуют все три поля')
+    if not line[0].isalpha():
+        raise NotNameError('поле имени содержит НЕ только буквы')
+    if ('@' not in line[1]) or ('.' not in line[1]):
+        raise NotEmailError('поле email НЕ содержит @ и .(точку)')
+    if int(line[2]) < 10 or int(line[2]) > 99:
+        raise ValueError('поле возраст НЕ является числом от 10 до 99')
+    else:
+        return line
 
-    for key, val in data.items():
-        if len(val) == 3:
-            if val[0].isalpha():
-                if '@' in val[1] and '.' in val[1]:
-                    if 10 < int(val[2]) < 99:
-                        with open('C:/Users/olivi/skills/HW9/registrations_good.log', 'ta', encoding="utf-8") as wf:
-                            wf.write(' '.join((val[0], val[1], val[2], '\n')))
-                    else:
-                        try:
-                            with open('C:/Users/olivi/skills/HW9/registrations_bad.log', 'ta', encoding="utf-8") as badf:
-                                badf.write(' '.join(('ValueError', val[0], val[1], val[2], '\n')))
-                            raise ValueError('поле возраст НЕ является числом от 10 до 99')
-                        except ValueError as exc:
-                            print(f'Поймано исключение: {exc}')
-                else:
-                    try:
-                        with open('C:/Users/olivi/skills/HW9/registrations_bad.log', 'ta', encoding="utf-8") as badf:
-                            badf.write(' '.join(('NotEmailError', val[0], val[1], val[2], '\n')))
-                        raise NotEmailError('поле email НЕ содержит @ и .(точку)')
-                    except NotEmailError as exc:
-                        print(f'Поймано исключение: {exc}')
-            else:
-                try:
-                    with open('C:/Users/olivi/skills/HW9/registrations_bad.log', 'ta', encoding="utf-8") as badf:
-                        badf.write(' '.join(('NotNameError', val[0], val[1], val[2], '\n')))
-                    raise NotNameError('поле имени содержит НЕ только буквы')
-                except NotNameError as exc:
-                    print(f'Поймано исключение: {exc}')
-        else:
+wf = open('C:/Users/olivi/skills/HW9/registrations_good.log', 'ta', encoding = 'utf-8')
+badf = open('C:/Users/olivi/skills/HW9/registrations_bad.log', 'ta', encoding = 'utf-8')
+with open('C:/Users/olivi/skills/HW9/registrations_.txt', 'r', encoding='utf-8') as rf:
+    for line in rf:
+        if not line.isspace():
             try:
-                with open('C:/Users/olivi/skills/HW9/registrations_bad.log', 'ta', encoding="utf-8") as badf:
-                    badf.write(' '.join(('ValueError', str(val).replace("[", "").replace("]", "").replace(",", "").replace("'", ""), '\n')))
-                raise ValueError('НЕ присутсвуют все три поля')
+                data = check_data(line.split())
+                wf.write(' '.join((data[0], data[1], data[2], '\n')))
             except ValueError as exc:
                 print(f'Поймано исключение: {exc}')
-
-try:
-    registration()
-except IOError:
-    print('Произошла ошибка ввода-вывода!')
+                badf.write(line)
+            except NotNameError as exc:
+                print(f'Поймано исключение: {exc}')
+                badf.write(line)
+            except NotEmailError as exc:
+                print(f'Поймано исключение: {exc}')
+                badf.write(line)
+wf.close()
+badf.close()
